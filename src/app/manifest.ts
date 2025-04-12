a95e8c71b3936a1cc596ad45f4685a96225b225a
nabeelcodes <nabeeldesigner91@hotmail.com>
2025-04-12 23:04:24 +0530

    migrated to manifest.ts from manifest.json

---
+++ src/app/manifest.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Web Archives",
+    short_name: "Webarc",
+    description:
+      "A curated digital library, featuring insightful articles on a wide range of web development topics.",
+    background_color: "#f9f2ed",
+    theme_color: "#f9f2ed",
+    start_url: "/",
+    dir: "ltr",
+    lang: "en",
+    orientation: "portrait",
+    icons: [
+      {
+        src: "https://www.pwabuilder.com/assets/icons/icon_192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any"
+      },
+      {
+        src: "https://www.pwabuilder.com/assets/icons/icon_512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable"
+      }
+    ]
+  };
+}
