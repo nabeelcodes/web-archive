2ff583b17ff11442a39c5ac63370464fe7aed5ff
nabeelcodes <nabeeldesigner91@hotmail.com>
2025-05-28 11:34:16 +0530

    Fixed critical vulnerability CWE_285
    - Updated nextjs to v15.2.3 to fix critical vulnerability CWE_285.
    - Added z-modal class to SearchWithVaul component for improved layering.

--- src/components/SearchWithVaul.tsx
+++ src/components/SearchWithVaul.tsx
@@ -22,7 +22,7 @@ const SearchWithVaul = ({ children, allTags, tagsInUrl, setTags }: SearchWithVau
       <Portal>
         <Overlay className='fixed inset-0 bg-black/40' />
 
-        <Content className='fixed bottom-0 left-0 right-0 h-fit bg-neutral-900 p-16 text-white outline-none'>
+        <Content className='fixed bottom-0 left-0 right-0 z-modal h-fit bg-neutral-900 p-16 text-white outline-none'>
           <Handle />
 
           <Title className='mt-20 text-center font-semibold'>Filter by tags</Title>
